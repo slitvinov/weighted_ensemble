c52b2ce386a2764be91e8ad58c13f1fc4effdd82
Sergey Litvinov <slitvinov@gmail.com>
2021-09-22 20:53:28 +0200

    up

--- split.c
+++ split.c
@@ -13,10 +13,11 @@ c99 split.c -O3 `pkg-config --libs --cflags gsl` -o split
 static const gsl_rng_type * T;
 static gsl_rng * r;
 enum {BUDGET = 100000};
-enum {NB = 10, NMAX = 1000};
+enum {NB = 10, NMAX = 100};
 enum {NP = NB * NMAX};
 static int bins[NB][NMAX];
 static int nb[NB];
+static const int n = 10; /* target in each bean */
 
 int main() {
   double dt;
@@ -25,10 +26,14 @@ int main() {
   double w[NP];
   double x[NP];
   double P[NB];
+  double wm;
   int i;
   int j;
   int k;
-  int n;
+  int N;
+  int l;
+  int m;
+  int o;
 
   gsl_rng_free(r);
   gsl_rng_env_setup();
@@ -38,13 +43,13 @@ int main() {
   F = -15.26;
   dt = 0.0001;
   sdt = sqrt(dt);
-  n = 10;
-  for (i = 0; i < n; i++) {
+  N = 10;
+  for (i = 0; i < N; i++) {
     x[i] = 0;
     w[i] = 1;
   }
   for (j = 0; j < BUDGET; ) {
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < N; i++) {
       x[i]  +=  F*dt + sqrt(2)*gsl_ran_gaussian(r, sdt);
       j ++;
       if (x[i] < 0)
@@ -56,7 +61,7 @@ int main() {
     /* sort into bins */
     for (i = 0; i < NB; i++)
       nb[i] = 0;
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < N; i++) {
       k = x[i] * NB;
       bins[k][nb[k]++] = i;
     }
@@ -68,10 +73,35 @@ int main() {
       for (k = 0; k < nb[i]; k++)
 	P[i] += w[bins[i][k]];
 
+    /* split */
+    for (i = 0; i < NB; i++)
+      for (k = 0; k < nb[i]; k++) {
+	l = bins[i][k];
+	if (w[l] > 2 * P[i] / n) {
+	  m = n * w[l] / P[i];
+	  if (n * w[l] > 2 * P[i] * m)
+	    m += 1;
+
+	  fprintf(stderr, "m = %d\n", m);
+
+	  wm = w[l]/m;
+	  for (o = 0; o < m - 1; o++) {
+	    if (N == NP) {
+	      fprintf(stderr, "split: N=%d = NP=%d\n", N, NP);
+	      exit(2);
+	    }
+	    x[N] = x[l];
+	    w[N] = wm;
+	    N++;
+	  }
+	  w[l] = wm;
+	}
+      }
+
     for (i = 0; i < NB; i++)
       printf("%d ", nb[i]);
     printf("\n");
-
+    
     for (i = 0; i < NB; i++)
       printf("%g ", P[i]);
     printf("\n");
