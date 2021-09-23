42f9d07be2b80df493882cef32e032271531878e
Sergey Litvinov <slitvinov@gmail.com>
2021-09-23 20:15:16 +0200

    up

--- bin.c
+++ bin.c
@@ -6,7 +6,7 @@
 
 static const gsl_rng_type * T;
 static gsl_rng * r;
-enum {N = 100};
+enum {N = 8};
 static unsigned long id[N];
 static unsigned long idx[N];
 static double x[N];
@@ -18,7 +18,7 @@ comp(const void *a, const void *b)
   const unsigned long *j;
   i = a;
   j = b;
-  return -1 ? x[*i] < x[*j] : (x[*i] > x[*j] ? 1 : 0) ;
+  return -1 ? x[*i] > x[*j] : (x[*i] < x[*j] ? 1 : 0) ;
 }
 
 static
@@ -39,11 +39,10 @@ int dump(const char *pattern, int step, unsigned long N) {
 
 int main() {
   double b;
+  double b_prev;
   double dt;
   double F;
   double sdt;
-  double td;
-  double tul;
   int i;
   int k;
   int step;
@@ -56,36 +55,30 @@ int main() {
   gid = 0;
 
   F = -15.26;
-  dt = 0.0001;
+  dt = 0.0005;
   sdt = sqrt(dt);
   for (i = 0; i < N; i++) {
     x[i] = 0;
     id[i] = gid++;
   }
+  b_prev = 0;
   for (step = 0; ; step ++) {
     for (i = 0; i < N; i++) {
       x[i]  +=  F*dt + sqrt(2)*gsl_ran_gaussian(r, sdt);
       if (x[i] < 0)
         x[i] = 0;
-      if (x[i] > 1)
+      if (x[i] > 0.5)
         goto stop;
     }
     dump("%08d.a.dat", step, N);
     for (i = 0; i < N; i++)
       idx[i] = i;
     qsort(idx, N, sizeof *idx, comp);
-
-    for (i = 0; i < N; i++) {
-      k = idx[i];
-      td  = x[i]; x[i] = x[k]; x[k] = td;
-      tul = id[i]; id[i] = id[k]; id[k] = tul;
-    }
-    
-    b = (x[N/2] + x[N/2 + 1])/2;
-    printf("%.16e\n", b);
+    b = (x[idx[N/2]] + x[idx[N/2 + 1]])/2;
+    printf("%.16e %.16e\n", b_prev, b);
+    b_prev = b;
     for (i = 0; i < N/2; i++) {
-      x[i] = x[N/2 + i];
-      id[i] = gid++;
+      x[idx[i]] = x[idx[N/2 + i]];
     }
     dump("%08d.b.dat", step, N);    
   }
--- bin.sh
+++ bin.sh
@@ -1,12 +1,16 @@
+c=1
 for i in *.[ab].dat
 do
+    b=`awk '2 * NR >= '$c'{print "\"\" " $2; exit}' b`
     o=`basename "$i" .dat`.png
     gnuplot < "$i" > "$o" -e '
 set term pngcairo size 1080, 1080 font "Helvetica,30" linewidth 5;
 unset key;
+unset ytics;
 set xlabel "position";
-set ylabel "ID";
-set ytics 250;
-plot [-0.05:0.5][-90:500] "-" u 3:1:(sqrt($4)/4) w circle fs transparent solid 0.1  border lt -1
+set xtics ('"$b"');
+set grid xtics linestyle -1;
+plot [-0.05:0.55][-0.5:7.5] "-" u 2:1:(0.025) w circle fs transparent solid 0.1  border lt -1
 ' || exit 2
+    c=$(($c+1))
 done
