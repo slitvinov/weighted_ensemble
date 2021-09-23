c81f91b692a3d08a46a55319da6fa09df912c733
Sergey Litvinov <slitvinov@gmail.com>
2021-09-23 13:21:47 +0200

    up

--- plot.sh
+++ plot.sh
@@ -5,6 +5,6 @@ do
 unset key;
 set xlabel "position";
 set ylabel "ID";
-plot [-0.05:0.5][-90:500] "-" u 3:1:(sqrt($4)/5) w circle fs transparent solid 0.1  border lt -1
+plot [-0.05:0.5][-90:800] "-" u 3:1:(sqrt($4)/4) w circle fs transparent solid 0.1  border lt -1 lw 2
 '
 done
