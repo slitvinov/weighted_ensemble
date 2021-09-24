8af14884c1bae106c7562e88b2cc21844e9e6538
Sergey Litvinov <slitvinov@gmail.com>
2021-09-24 13:07:14 +0200

    up

--- main.c
+++ main.c
@@ -6,12 +6,12 @@ static const char *me = "main";
 static const gsl_rng_type * T;
 static gsl_rng * r;
 enum {BUDGET = 160000000};
-enum {NB = 10, NMAX = 10000};
-enum {NP = NB * NMAX};
+enum {BMAX = 100, NMAX = 1000};
+enum {NP = BMAX * NMAX};
 static double w[NP];
 static double x[NP];
-static int bins[NB][NMAX];
-static int nb[NB];
+static int bins[BMAX][NMAX];
+static int nb[BMAX];
 static unsigned long id[NP];
 static unsigned long pid[NP];
 
@@ -41,12 +41,12 @@ main(int argc, char **argv)
   char *end;
   double dt;
   double F;
-  double P[NB];
   double sdt;
   double u;
   double w0;
   double w1;
   double wm;
+  double *P;
   int i0;
   int i1;
   int j;
@@ -55,6 +55,7 @@ main(int argc, char **argv)
   int l;
   int m;
   int Nflag;
+  int Bflag;
   int nnew;
   int o;
   long double cumflux;
@@ -63,8 +64,10 @@ main(int argc, char **argv)
   unsigned long i;
   unsigned long N;
   unsigned long step;
+  unsigned long b;
 
   Nflag = 0;
+  Bflag = 0;
   while (*++argv != NULL && argv[0][0] == '-')
     switch (argv[0][1]) {
     case 'h':
@@ -73,7 +76,7 @@ main(int argc, char **argv)
     case 'n':
       argv++;
       if (argv[0] == NULL) {
-	fprintf(stderr, "%s: -o needs an argument\n", me);
+	fprintf(stderr, "%s: -n needs an argument\n", me);
 	return 1;
       }
       n = strtol(argv[0], &end, 10);
@@ -84,6 +87,20 @@ main(int argc, char **argv)
       }
       Nflag = 1;
       break;
+    case 'b':
+      argv++;
+      if (argv[0] == NULL) {
+	fprintf(stderr, "%s: -b needs an argument\n", me);
+	return 1;
+      }
+      b = strtol(argv[0], &end, 10);
+      if (errno != 0 || *end != '\0') {
+	fprintf(stderr, "%s: -b argument is not an integer '%s'\n",
+		me, argv[0]);
+	return 1;
+      }
+      Bflag = 1;
+      break;
     default:
       fprintf(stderr, "%s: unknown option '%s'\n", me, argv[0]);
       return 1;
@@ -91,6 +108,15 @@ main(int argc, char **argv)
 
   if (Nflag == 0) {
     fprintf(stderr, "%s: -n is not set\n", me);
+    return 1;
+  }
+  if (Bflag == 0) {
+    fprintf(stderr, "%s: -b is not set\n", me);
+    return 1;
+  }
+
+  if ((P = malloc(b * sizeof *P)) == NULL) {
+    fprintf(stderr, "%s: malloca failed\n", me);
     return 1;
   }
 
@@ -103,7 +129,7 @@ main(int argc, char **argv)
   F = -15.76;
   dt = 0.0001;
   sdt = sqrt(dt);
-  N = n * NB;
+  N = n * b;
   for (i = 0; i < N; i++) {
     x[i] = 0;
     w[i] = 1.0/N;
@@ -125,10 +151,10 @@ main(int argc, char **argv)
     }
 
     /* sort into bins */
-    for (i = 0; i < NB; i++)
+    for (i = 0; i < b; i++)
       nb[i] = 0;
     for (i = 0; i < N; i++) {
-      k = x[i] * NB;
+      k = x[i] * b;
       bins[k][nb[k]] = i;
       if (nb[k] == NMAX) {
 	fprintf(stderr, "%s: nb[k]=%d = NMAX=%d\n", me, nb[k], NMAX);
@@ -138,14 +164,14 @@ main(int argc, char **argv)
     }
 
     /* prob */
-    for (i = 0; i < NB; i++)
+    for (i = 0; i < b; i++)
       P[i] = 0;
-    for (i = 0; i < NB; i++)
+    for (i = 0; i < b; i++)
       for (k = 0; k < nb[i]; k++)
 	P[i] += w[bins[i][k]];
 
     /* split */
-    for (i = 0; i < NB; i++)
+    for (i = 0; i < b; i++)
       for (k = 0; k < nb[i]; k++) {
 	l = bins[i][k];
 	if (w[l] > 2 * P[i] / n) {
@@ -170,13 +196,13 @@ main(int argc, char **argv)
       }
 
     /* sort bins */
-    for (i = 0; i < NB; i++)
+    for (i = 0; i < b; i++)
       qsort(bins[i], nb[i], sizeof *bins[i], comp);
 
     /* merge */
     for (i = 0; i < N; i++)
       keep[i] = 1;
-    for (i = 0; i < NB; i++) {
+    for (i = 0; i < b; i++) {
       for (k = 0; k + 1 < nb[i]; k += 2) {
 	i0 = bins[i][k];
 	i1 = bins[i][k + 1];
@@ -205,8 +231,10 @@ main(int argc, char **argv)
 	k++;
       }
     N = k;
-    if (step % 100 == 0)
-      printf("%.16e %.16Le\n", step * dt, cumflux);
+    if (step % 1000 == 0)
+      printf("%.16e %.16Le %.16Le\n", step * dt, cumflux, step ? cumflux / step / dt : 0);
     fflush(stdout);
   }
+
+  free(P);
 }
